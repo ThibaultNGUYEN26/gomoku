645fa94bf7c1e7eab4bdc0d89ee6ef62b74dc93d
Thibault <118262807+ThibaultNGUYEN26@users.noreply.github.com>
2025-10-24 14:11:56 +0200

    Refactor Menu component to use span elements

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,18 +1,18 @@
-export default function Menu({ onPlay }){
+export default function Menu({ onPlay }) {
   return (
     <div className="falling-container">
-      <div className="falling black1">⚫</div>
-      <div className="falling white1">⚪</div>
-      <div className="falling black2">⚫</div>
-      <div className="falling white2">⚪</div>
-      <div className="falling black3">⚫</div>
-      <div className="falling white3">⚪</div>
-      <div className="falling black4">⚫</div>
-      <div className="falling white4">⚪</div>
-      <div className="falling black5">⚫</div>
-      <div className="falling white5">⚪</div>
-      <div className="falling black6">⚫</div>
-      <div className="falling white6">⚪</div>
+      <div className="falling black1"><span className="menu-stone black" aria-hidden="true" /></div>
+      <div className="falling white1"><span className="menu-stone white" aria-hidden="true" /></div>
+      <div className="falling black2"><span className="menu-stone black" aria-hidden="true" /></div>
+      <div className="falling white2"><span className="menu-stone white" aria-hidden="true" /></div>
+      <div className="falling black3"><span className="menu-stone black" aria-hidden="true" /></div>
+      <div className="falling white3"><span className="menu-stone white" aria-hidden="true" /></div>
+      <div className="falling black4"><span className="menu-stone black" aria-hidden="true" /></div>
+      <div className="falling white4"><span className="menu-stone white" aria-hidden="true" /></div>
+      <div className="falling black5"><span className="menu-stone black" aria-hidden="true" /></div>
+      <div className="falling white5"><span className="menu-stone white" aria-hidden="true" /></div>
+      <div className="falling black6"><span className="menu-stone black" aria-hidden="true" /></div>
+      <div className="falling white6"><span className="menu-stone white" aria-hidden="true" /></div>
 
       <header className="header-bar">
         <h1 className="gomoku-title">GOMOKU</h1>
@@ -24,7 +24,7 @@ export default function Menu({ onPlay }){
       </main>
 
       <footer className="footer-bar">
-        Made with ⚫⚪ by cleblais &amp; thibnguy
+        Made with <span className="inline-stone black" aria-hidden="true" /><span className="inline-stone white" aria-hidden="true" /> by cleblais &amp; thibnguy
       </footer>
     </div>
   );
